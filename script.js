ac2e946f2523a3b4a52b30dba0661afafdc0732d
ElBurritoLex <alex.arr.mtz@gmail.com>
2025-12-04 23:10:22 -0600

    Added new console.log functions to script.js and edited message on index.html

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 alert("Welcome to Purry Lovers!");
 console.log("Purry Lovers script loaded successfully.");
+console.log("Hooray! The script is running without any errors.");
+console.warn("This is a warning message. Check your code for potential issues.");
+console.error("This is an error message. Something went wrong!");
+console.info("This is an informational message. Everything is running smoothly.");
